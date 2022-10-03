730d4876493e90eeb51a3aae021ed8c1909ad830
Ivan Volkov <74770068+iivolkoff-art@users.noreply.github.com>
2022-10-03 16:35:39 +0300

    Add files via upload

---
+++ carsdb.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cars
+(Id INTEGER PRIMARY KEY,
+Name VARCHAR(20),
+Price INT);
+
+INSERT INTO cars
+VALUES
+(1,'Audi',52642),
+(2,'Mercedes',57127),
+(3,'Skoda',9000),
+(4,'Volvo',29000),
+(5,'Bentley',350000),
+(6,'Citroen',21000),
+(7,'Hummer',41400),
+(8,'Volkswagen',21600)
